8df78b15056017b7167c4b5609aa3b4dec277abb
Josh Daniel <joshxfi.dev@gmail.com>
2023-12-05 22:40:52 +0800

    feat: display currently serving

--- src/app/queue/status/page.tsx
+++ src/app/queue/status/page.tsx
@@ -1,39 +1,55 @@
 "use client";
 
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-import { doc, updateDoc } from "firebase/firestore";
-import { useDocument } from "react-firebase-hooks/firestore";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { useCollection, useDocument } from "react-firebase-hooks/firestore";
 
 import { db } from "@/lib/firebase";
 import { QueueData } from "@/types";
 import { Loader } from "@/components";
-import { Card, Button, QueueContainer } from "@/components/utils";
-
-const queues = [
-  {
-    heading: "SA-29",
-    subheading: "Cashier 1",
-  },
-  {
-    heading: "SA-30",
-    subheading: "Cashier 2",
-  },
-  {
-    heading: "SA-31",
-    subheading: "Cashier 3",
-  },
-];
+import { Card, Button, QueueContainer, Modal } from "@/components/utils";
+import { toast } from "sonner";
 
 export default function Status() {
+  const router = useRouter();
   const searchParams = useSearchParams();
+  const queueId = searchParams.get("id") ?? "";
+  const [modalOpen, setModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const queueId = searchParams.get("id");
-
-  const [value, loading] = useDocument(doc(db, "queue", queueId ?? "default"));
+  const [value, loading] = useDocument(doc(db, "queue", queueId));
+  const [serving, _loading] = useCollection(
+    query(collection(db, "queue"), where("status", "==", "serving"))
+  );
   const { name, queueNumber, status } = (value?.data() as QueueData) ?? {};
 
+  const handleCancel = async () => {
+    try {
+      setModalOpen(false);
+      setIsLoading(true);
+      await deleteDoc(doc(db, "queue", queueId));
+      router.push("/queue/scan");
+      setIsLoading(false);
+    } catch (e: any) {
+      toast.error(e.message);
+    }
+  };
+
   useEffect(() => {
+    if (!loading && !value?.exists()) {
+      toast.error("Queue does not exist.");
+      router.push("/queue/scan");
+      return;
+    }
+
     if (queueId && status === "fresh") {
       (async () => {
         await updateDoc(doc(db, "queue", queueId), {
@@ -41,42 +57,67 @@ export default function Status() {
         });
       })();
     }
-  }, [queueId, status]);
+  }, [queueId, status, loading, value?.exists]);
 
   return (
     <QueueContainer title="Current Status">
-      {loading ? (
-        <Loader className="h-12 w-12 mt-32 mx-auto" />
-      ) : (
-        <>
-          <div className="bg-secondary-200 rounded-xl mt-12 p-12 text-center">
-            <h1 className="text-4xl mb-4">{queueNumber}</h1>
-            <p className="text-zinc-400">{name}</p>
-          </div>
+      <Modal
+        isOpen={modalOpen}
+        title="Delete Queue"
+        description="Are you sure you want to give up your queue position?"
+        onClose={() => setModalOpen(false)}
+        handleConfirm={[
+          {
+            text: "Yes, I'm sure",
+            fn: handleCancel,
+          },
+        ]}
+      />
 
-          <div className="h-2 mt-3 bg-primary-200 rounded-full w-[60%]"></div>
-          <p className="text-sm text-zinc-400 italic mt-2">
-            Estimated time: 13 mins left
-          </p>
+      <div className="min-h-screen">
+        {loading || isLoading ? (
+          <Loader className="h-12 w-12 mt-32 mx-auto" />
+        ) : (
+          <>
+            <div className="bg-secondary-200 rounded-xl mt-12 p-12 text-center">
+              <h1 className="text-4xl mb-4">{queueNumber}</h1>
+              <p className="text-zinc-400">{name}</p>
+            </div>
 
-          <p className="mb-3 mt-24 text-zinc-400 text-sm">Currently Serving</p>
-          <div className="space-y-4">
-            {queues.map((q) => (
-              <Card
-                key={q.heading}
-                heading={q.heading}
-                subheading={q.subheading}
-              />
-            ))}
-          </div>
+            <div className="h-2 mt-3 bg-primary-200 rounded-full w-[60%]"></div>
+            <p className="text-sm text-zinc-400 italic mt-2">
+              Estimated time: 13 mins left
+            </p>
 
-          <div className="mt-8">
-            <Button dark action="/">
-              Cancel Queue
-            </Button>
-          </div>
-        </>
-      )}
+            {serving?.docs && serving.docs.length > 0 && (
+              <>
+                <p className="mb-3 mt-24 text-zinc-400 text-sm">
+                  Currently Serving
+                </p>
+                <div className="space-y-4">
+                  {serving?.docs.map((doc) => {
+                    const data = doc.data() as QueueData;
+
+                    return (
+                      <Card
+                        key={doc.id}
+                        heading={`${data.queueNumber.toString()} (${
+                          data.name
+                        })`}
+                        subheading="Cashier 1"
+                      />
+                    );
+                  })}
+                </div>
+              </>
+            )}
+
+              <Button className="mt-8 w-full font-normal" action={() => setModalOpen(true)}>
+                Cancel Queue
+              </Button>
+          </>
+        )}
+      </div>
     </QueueContainer>
   );
 }
